abbee6f22762f1808e626bea722567c96b381945
manujunqueira <manuelajunqueira27@gmail.com>
2024-09-11 12:01:36 -0400

    "Create displayProductDetails function"

--- inventory_management.js
+++ inventory_management.js
@@ -1,4 +1,5 @@
-// Sample Data for Inventory Initialization
+// Task 1 - Create an Inventory Array of Product Objects
+
 
 const inventory = [
     { name: 'Laptop', price: 1200, quantity: 10, lowStockLevel: 3 },
@@ -6,7 +7,17 @@ const inventory = [
     { name: 'Tablet', price: 400, quantity: 7, lowStockLevel: 1 },
     { name: 'Headphones', price: 100, quantity: 15, lowStockLevel: 5 },
     { name: 'Smartwatch', price: 250, quantity: 3, lowStockLevel: 1 }
-];
+]; //Sample Data for Inventory Initialization
 
-// Task 1 - Create an Inventory Array of Product Objects
+// Task 2 - Create a Function to Display Product Details
+
+function displayProductDetails (product) {
+    const stockStatus = product.quantity <= product.lowStockLevel ? 'Low Stock' : 'In Stock';
+    console.log(`Product: ${product.name}`);
+    console.log(`Price: $${product.price}`);
+    console.log(`Quantity in Stock: ${product.quantity}`);
+    console.log(`Stock Status: ${stockStatus}`);
+}
+
+
 
